4d6341c4defa7e0e724ad4f853699bd87e738058
user <user@WIN-CP209TC39O5>
2026-03-22 19:28:53 +0300

    Connectror begin

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Globalization;
 
 namespace ADO
 {
@@ -12,34 +13,20 @@ namespace ADO
         static void Main(string[] args)
         {
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            Console.WriteLine(connection_string);
 
-            SqlConnection connection = new SqlConnection(connection_string);
-            connection.Open();
+            Connector connector = new Connector (connection_string);
 
             string cmd = "SELECT movie_id, title, release_date, first_name, last_name FROM Movies, Directors WHERE director=director_id";
-            SqlCommand command = new SqlCommand(cmd, connection);
 
-            SqlDataReader reader = command.ExecuteReader();
-            for(int i=0; i < reader.FieldCount; i++)
-            {
-                Console.Write(reader.GetName(i) + "\t");
-            }
-            Console.WriteLine();
-            while (reader.Read())
-            {
-                //Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
-                for (int i = 0; i < reader.FieldCount; i++)
-                {
-                    Console.Write($"{reader[i]}\t");
-                }
-                Console.WriteLine();
-            }
-            reader.Close();
+            connector.Select(cmd);
+            Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Movies")}");
 
-            command.CommandText = "SELECT COUNT (*) FROM Movies";
-            Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
-            connection.Close();
+            connector.Select("SELECT * FROM Directors");
+            Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
+
+            //command.CommandText = "SELECT COUNT (*) FROM Movies";
+            //Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
+            //connection.Close();
         }
     }
 }
